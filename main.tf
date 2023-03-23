16df88875373ddb24c340fc7e082e249f6306c75
kleung <kleung134@gmail.com>
2023-03-23 19:45:12 +0000

    Added the labels, remove tags

--- main.tf
+++ main.tf
@@ -67,10 +67,12 @@ resource "google_compute_instance" "hashicat" {
     ssh-keys = "ubuntu:${chomp(tls_private_key.ssh-key.public_key_openssh)} terraform"
   }
 
-  tags = ["http-server", "devops", "billable"]
+  tags = ["http-server"]
 
   labels = {
     name = "hashicat"
+    department = "devops"
+    billing = "true"
   }
 
 }
